9088073bfe23ac84163253afde63b43d7de095ab
chen-huai <52665471+chen-huai@users.noreply.github.com>
2024-03-12 17:29:43 +0800

    自动操作电脑界面

--- Auto_Operate.py
+++ Auto_Operate.py
@@ -20,6 +20,7 @@ from File_Operate import *
 from Auto_Operate_Ui import Ui_MainWindow
 from Data_Table import *
 from Logger import *
+from Windows_Operate import *
 
 
 class MyMainWindow(QMainWindow, Ui_MainWindow):
@@ -34,9 +35,9 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         self.actionAuthor.triggered.connect(self.showAuthorMessage)
         self.pushButton_12.clicked.connect(self.textBrowser.clear)
         self.pushButton_16.clicked.connect(self.getFileUrl)
-        self.pushButton_49.clicked.connect(self.viewOdmData)
-        self.pushButton_56.clicked.connect(lambda: self.orderUnlockOrLock('Unlock'))
-        self.pushButton_57.clicked.connect(lambda: self.orderUnlockOrLock('Lock'))
+        self.pushButton_49.clicked.connect(self.viewData)
+        self.pushButton_56.clicked.connect(self.getCoordinates)
+        self.pushButton_57.clicked.connect(self.win_auto_step)
         self.filesUrl = []
 
     def getConfig(self):
@@ -56,7 +57,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         oneWeekday = (datetime.datetime.now() + datetime.timedelta(days=7)).strftime('%Y.%m.%d')
         desktopUrl = os.path.join(os.path.expanduser("~"), 'Desktop')
         configFileUrl = '%s/config' % desktopUrl
-        configFile = os.path.exists('%s/config_sap_controlling.csv' % configFileUrl)
+        configFile = os.path.exists('%s/config_auto.csv' % configFileUrl)
         # print(desktopUrl,configFileUrl,configFile)
         if not configFile:  # 判断是否存在文件夹如果不存在则创建为文件夹
             reply = QMessageBox.question(self, '信息', '确认是否要创建配置文件', QMessageBox.Yes | QMessageBox.No, QMessageBox.Yes)
@@ -74,7 +75,7 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
     # 获取配置文件内容
     def getConfigContent(self):
         # 配置文件
-        csvFile = pd.read_csv('%s/config_sap_controlling.csv' % configFileUrl, names=['A', 'B', 'C'])
+        csvFile = pd.read_csv('%s/config_auto.csv' % configFileUrl, names=['A', 'B', 'C'])
         global configContent
         global username
         global role
@@ -102,15 +103,24 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
 
         configContent = [
             ['特殊开票', '内容', '备注'],
-            ['SAP_Date_URL', 'N:\\XM Softlines\\6. Personel\\5. Personal\\Supporting Team\\收样\\3.Sap\\ODM Data - XM',
-             '文件数据路径'],
+            ['Step_Data_URL', desktopUrl, '流程步骤数据路径'],
+            ['文件说明', '序号，步骤，备注', '文件列头说明'],
+            ['步骤说明', 'move,x,y,time', '移动鼠标'],
+            ['步骤说明', 'click,x,y', '点击鼠标'],
+            ['步骤说明', 'dragTo,x,y,time', '拖动鼠标'],
+            ['步骤说明', 'mouseDown,x,y,button', '按下鼠标'],
+            ['步骤说明', 'mouseUp,x,y,button', '释放鼠标'],
+            ['步骤说明', 'typewrite,****', '键盘输入'],
+            ['步骤说明', 'press,button', '模拟按下并释放按键'],
+            ['步骤说明', 'hotkey,button1,button2', '模拟按下热键并释放按键'],
+            ['步骤说明', 'sleep,time', '暂停时间'],
         ]
         config = np.array(configContent)
         df = pd.DataFrame(config)
-        df.to_csv('%s/config_sap_controlling.csv' % configFileUrl, index=0, header=0, encoding='utf_8_sig')
+        df.to_csv('%s/config_auto.csv' % configFileUrl, index=0, header=0, encoding='utf_8_sig')
         self.textBrowser.append("配置文件创建成功")
         QMessageBox.information(self, "提示信息",
-                                "默认配置文件已经创建好，\n如需修改请在用户桌面查找config文件夹中config_sap_controlling.csv，\n将相应的文件内容替换成用户需求即可，修改后记得重新导入配置文件。",
+                                "默认配置文件已经创建好，\n如需修改请在用户桌面查找config文件夹中config_auto.csv，\n将相应的文件内容替换成用户需求即可，修改后记得重新导入配置文件。",
                                 QMessageBox.Yes)
 
     # 导出配置文件
@@ -141,16 +151,23 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
         QMessageBox.about(self, "版本",
                           "V 24.01\n\n\n 2024-02-01")
 
+    # 获取坐标
+    def getCoordinates(self):
+        win_obj = Windows_auto()
+        time = int(self.spinBox.text())
+        coordinates = win_obj.get_desktop_coordinates(time)
+        self.textBrowser.append("坐标：(%s,%s)" % (coordinates[0], coordinates[1]))
+        return coordinates
 
     # 获取文件
     def getFile(self):
         selectBatchFile = QFileDialog.getOpenFileName(self, '选择ODM导出文件',
-                                                      '%s\\%s' % (configContent['SAP_Date_URL'], today),
+                                                      '%s\\%s' % (configContent['Step_Data_URL'], today),
                                                       'files(*.docx;*.xls*;*.csv)')
         fileUrl = selectBatchFile[0]
         return fileUrl
 
-    # SAP数据路径
+    # 步骤数据路径
     def getFileUrl(self):
         fileUrl = MyMainWindow.getFile(self)
         if fileUrl:
@@ -160,69 +177,78 @@ class MyMainWindow(QMainWindow, Ui_MainWindow):
             self.textBrowser.append("请重新选择ODM文件")
             QMessageBox.information(self, "提示信息", "请重新选择ODM文件", QMessageBox.Yes)
 
-
-    # 获取billing list文件
-    def getBillingListFile(self):
+    # 查看SAP操作数据详情
+    def viewData(self):
         try:
-            selectBatchFile = QFileDialog.getOpenFileName(self, '选择文件',
-                                                          '%s' % configContent['Billing_List_URL'],
-                                                          'files(*.xlsx)')
-            fileUrl = selectBatchFile[0]
-            if fileUrl:
-                self.lineEdit_25.setText(fileUrl)
-                app.processEvents()
-                self.textBrowser_3.append('选中Billing List文件：%s' % fileUrl)
-                self.textBrowser_3.append('----------------------------------')
-            else:
-                self.textBrowser_3.append('无选中文件')
-                self.textBrowser_3.append('----------------------------------')
-            app.processEvents()
-            return fileUrl
+            fileUrl = self.lineEdit_6.text()
+            odm_data_obj = Get_Data()
+            df = odm_data_obj.getFileData(fileUrl)
+            myTable.createTable(df)
+            myTable.showMaximized()
         except Exception as errorMsg:
-            self.textBrowser_3.append("<font color='red'>出错信息：%s </font>" % errorMsg)
+            self.textBrowser.append("<font color='red'>出错信息：%s </font>" % errorMsg)
             app.processEvents()
             return
 
-    # 获取billing数据
-    def getBillingListData(self, sheet_name=[]):
+    # 获取b步骤数据
+    def getStepListData(self):
         try:
-            billing_list_url = self.lineEdit_25.text()
-            if billing_list_url == '':
-                self.textBrowser_3.append('无选中文件')
-                self.textBrowser_3.append('----------------------------------')
+            Step_Data_URL = self.lineEdit_6.text()
+            if Step_Data_URL == '':
+                self.textBrowser.append('无选中文件')
+                self.textBrowser.append('----------------------------------')
                 app.processEvents()
                 return None
             else:
-                billing_list_obj = Get_Data()
-                billing_list_data = billing_list_obj.getFileMoreSheetData(billing_list_url, sheet_name)
-                pivotTableKey = ['Final Invoice No.', 'Customer Name', 'CS', 'Cur.']
-                valusKey = ['求和项:Amount with VAT']
-                billing_list_data = billing_list_obj.pivotTable(pivotTableKey, valusKey)
-                billing_list_data = billing_list_data.reset_index()
-                return billing_list_data
+                step_list_obj = Get_Data()
+                step_list_data = step_list_obj.getFileData(Step_Data_URL)
+                return step_list_data
         except Exception as errorMsg:
-            self.textBrowser_3.append("<font color='red'>出错信息：%s </font>" % errorMsg)
+            self.textBrowser.append("<font color='red'>出错信息：%s </font>" % errorMsg)
             app.processEvents()
             return
 
-
-    # 查看Billing list数据
-    def viewBillingListData(self):
-        fileUrl = self.lineEdit_25.text()
-        if fileUrl:
-            sheet_name = []
-            df = myWin.getBillingListData(sheet_name)
-            try:
-                myTable.createTable(df)
-                myTable.showMaximized()
-            except Exception as errorMsg:
-                self.textBrowser_3.append('数据有问题%s' % errorMsg)
-                self.textBrowser_3.append('----------------------------------')
+    # 电脑桌面操作
+    def win_auto_step(self):
+        steps_list_data = myWin.getStepListData()
+        steps_num = list(steps_list_data['序号'])
+        steps = list(steps_list_data['步骤'])
+        win_auto_obj = Windows_auto()
+        cycles_num = int(self.spinBox_2.text())
+        cycle_msg = {}
+        step_msg = {}
+        for cycle in range(cycles_num):
+            cycle_msg['flag'] = 1
+            self.textBrowser.append('第%s次循环步骤' % (cycle+1))
+            flag = 1
+            if self.checkBox.isChecked():
+                reply = QMessageBox.question(self, '信息', '是否继续操作电脑桌面！！！',
+                                             QMessageBox.Yes | QMessageBox.No,
+                                             QMessageBox.Yes)
+                if reply == QMessageBox.No:
+                    flag = 0
+            if flag == 1:
+                num = 1
+                step_msg['flag'] = 1
+                for step in steps:
+                    step_flag = 1
+                    step_list = step.split(',')
+                    step_flag = win_auto_obj.window_automation_operation(step_list)
+                    if step_flag == 0:
+                        step_msg['flag'] = 0
+                        cycle_msg['flag'] = 0
+                        step_msg['information'] = '第%s步骤失败' % num
+                        self.textBrowser.append("<font color='red'> 第%s步骤失败 </font>" % num)
+                        app.processEvents()
+                    num += 1
+            else:
+                self.textBrowser.append('跳过第%s次循环步骤' % (cycle + 1))
+            if cycle_msg['flag'] == 1:
+                self.textBrowser.append('成功')
                 app.processEvents()
-        else:
-            self.textBrowser_3.append('无选中文件')
-            self.textBrowser_3.append('----------------------------------')
-            app.processEvents()
+            else:
+                self.textBrowser.append("<font color='red'> 失败 </font>" % num)
+
 
 
 if __name__ == "__main__":
---
+++ Windows_Operate.py
@@ -0,0 +1,43 @@
+import pyautogui
+import time
+
+
+class Windows_auto():
+    def get_desktop_coordinates(self, second):
+        time.sleep(second)
+        # x屏幕横轴，y屏幕纵轴
+        x, y = pyautogui.position()
+        return (x, y)
+    def window_automation_operation(self, step):
+        # step是个列表
+        flag = 1
+        try:
+            if step[0].lower() == 'move':
+                pyautogui.moveTo(int(step[1]), int(step[2]), duration=float(step[3]))
+            elif step[0].lower() == 'click':
+                pyautogui.click(int(step[1]), int(step[2]))
+            elif step[0].lower() == 'dragto':
+                pyautogui.dragTo(int(step[1]), int(step[2]), duration=float(step[3]))
+            elif step[0].lower() == 'mousedown':
+                pyautogui.mouseDown(int(step[1]), int(step[2]), duration=step[3])
+            elif step[0].lower() == 'mouseup':
+                pyautogui.mouseUp(int(step[1]), int(step[2]), duration=step[3])
+            elif step[0].lower() == 'typewrite':
+                pyautogui.typewrite(step[1])
+            elif step[0].lower() == 'sleep':
+                time.sleep(int(step[1]))
+            elif step[0].lower() == 'press':
+                pyautogui.press(step[1])
+            elif step[0].lower() == 'hotkey':
+                i = 0
+                buttons = ''
+                for button in step:
+                    if i != 0:
+                        buttons += button
+                    i += 1
+                pyautogui.hotkey(step[1], step[2])
+            else:
+                time.sleep(3)
+        except:
+            flag = 0
+        return flag
